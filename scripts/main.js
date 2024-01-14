4f5ddb2d577a37ad62d145d8bd179fbccf2d17b5
Renan Santos <renansantos7@proton.me>
2024-01-14 16:13:49 -0300

    comito inicial

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+const downloadBtn = document.querySelector('.hero__cta__download-btn')
+
+downloadBtn.href = 'http://adfoc.us/serve/sitelinks/?id=475250&url=http://optifine.net/adloadx?f=OptiFine_1.20.1_HD_U_I6.jar&x=f8f1'
