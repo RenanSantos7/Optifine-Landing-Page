b5b0c4f1a389b36c287584c12b4814017f516208
Renan Santos <renansantos7@proton.me>
2024-01-18 17:52:55 -0300

    Other downloads in tables

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,21 @@ import handleDetails from "./details.js"
 
 handleDetails()
 
+// % MAIN DOWNLOAD BUTTON LINK 
 const downloadBtn = document.querySelector('.hero__cta__download-btn')
 
 downloadBtn.href = 'http://adfoc.us/serve/sitelinks/?id=475250&url=http://optifine.net/adloadx?f=OptiFine_1.20.1_HD_U_I6.jar&x=f8f1'
+
+
+// % CHANGE HERO BACKGROUND 
+const elementHero = document.querySelector('.hero')
+
+const now = new Date()
+
+if (now.getHours < 12) {
+    elementHero.style.backgroundImage = 'url("../assets/images/bg/swamp-river.jpg")'
+} else if (12 <= now.getHours() < 18) {
+    elementHero.style.backgroundImage = 'url("../assets/images/bg/sunset.jpg")'
+} else {
+    elementHero.style.backgroundImage = 'url("../assets/images/bg/sunshine-swamp.jpg")'
+}
